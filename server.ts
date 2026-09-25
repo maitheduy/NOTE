d4b399a56d7c51d0bfa48a49bbc9528c61b4da22
Mai The Duy <vnwfpb@gmail.com>
2026-09-26 00:38:07 +0700

    Make server Render-compatible

--- server.ts
+++ server.ts
@@ -87,7 +87,7 @@ async function startServer() {
 
   app.use(vite.middlewares);
 
-  const port = 3000;
+  const port = Number(process.env.PORT) || 3000;
   app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${port}`);
   });
